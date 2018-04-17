e674d1547503591aab0a3acd68a134a64b1994a7
geoffrey1218 <geoff.s.rich@gmail.com>
2018-04-17 14:11:33 -0700

    Remove unused code

--- spectrogram.py
+++ spectrogram.py
@@ -4,22 +4,10 @@ import sunau
 from scipy import signal
 from scipy.io import wavfile
 
-def stereo_to_mono(audiodata):
-    '''
-    From https://stackoverflow.com/questions/30401042/stereo-to-mono-wav-in-python
-    '''
-    newaudiodata = []
-
-    for i in range(len(audiodata)):
-        d = (audiodata[i][0] + audiodata[i][1])/2
-        newaudiodata.append(d)
-
-    return np.array(newaudiodata, dtype='int16')
-
 def get_spectrogram(filename):
-    # sample_rate, samples = wavfile.read(filename)
-    # samples = stereo_to_mono(samples)
-    # wavfile.write('test_mono.wav', sample_rate, samples)
+    '''
+    Help from https://stackoverflow.com/questions/44787437/how-to-convert-a-wav-file-to-a-spectrogram-in-python3
+    '''
     f = sunau.open(filename, 'r')
     num_frames = f.getnframes()
     sample_rate = f.getframerate()
