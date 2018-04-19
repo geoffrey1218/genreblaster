7b93e6fdd36c788d5df8924246bf18706ea5fe8c
geoffrey1218 <geoff.s.rich@gmail.com>
2018-04-19 13:31:33 -0700

    Make spectrograms using sox

--- spectrogram.py
+++ spectrogram.py
@@ -1,26 +1,28 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import sunau
-from scipy import signal
-from scipy.io import wavfile
+import os
 
-def get_spectrogram(filename):
-    '''
-    Help from https://stackoverflow.com/questions/44787437/how-to-convert-a-wav-file-to-a-spectrogram-in-python3
-    '''
-    f = sunau.open(filename, 'r')
-    num_frames = f.getnframes()
-    sample_rate = f.getframerate()
-    audio_data = np.fromstring(f.readframes(num_frames), dtype=np.int16) 
-    frequencies, times, spectrogram = signal.spectrogram(audio_data, sample_rate)
 
-    plt.pcolormesh(times, frequencies, spectrogram)
-    plt.imshow(spectrogram)
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    plt.show()
+def make_spectrograms():
+    cwd = os.getcwd()
+    genres_folder = os.path.join(cwd, 'genres')
+    spectrogram_folder = os.path.join(cwd, 'spectrograms')
+    try:
+        os.mkdir(spectrogram_folder)
+    except FileExistsError:
+        pass 
+    
+    for subdir, dirs, files in os.walk(genres_folder):
+        for file in files:
+            if file.endswith('.au'):
+                genre_folder = os.path.join(spectrogram_folder, os.path.basename(subdir))
+                try:
+                    os.mkdir(genre_folder)
+                except FileExistsError:
+                    pass 
 
+                current_filepath = os.path.join(subdir, file)
+                spectrogram_output = os.path.join(genre_folder, f'{file}.png')
+                os.system(f'sox {current_filepath} -n spectrogram -Y 200 -X 50 -m -r -o {spectrogram_output}')
 
 
 if __name__ == '__main__':
-    get_spectrogram('test.au')
+    make_spectrograms()
