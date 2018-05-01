b1296b4fa817c1f64d4add9fe6b828ea35a88d86
Michael Gamlem III <mgamlem3@gmail.com>
2018-05-01 14:03:51 -0700

    Modified to get 128x128 images

--- slices.py
+++ slices.py
@@ -4,8 +4,8 @@ import os
 
 
 def make_slices():
-    height = 129
-    width = 150
+    height = 128
+    width = 128
 
     cwd = os.getcwd()
     spectrogram_folder = os.path.join(cwd, 'spectrograms')
@@ -29,16 +29,16 @@ def make_slices():
                 # slice
                 img = Image.open(current_filepath)
                 imgwidth, imgheight = img.size
-                for i in range(0, imgheight, height):
-                    for j in range(0, imgwidth-width, width):
-                        box = (j, i, j + width, i + height)
-                        a = img.crop(box)
-                        try:
-                            o = a
-                            strfile = file.rstrip(".au.png")
-                            o.save(os.path.join(spectrogram_folder, strfile + "_" + str(j) + ".png"))
-                        except Exception as e:
-                            print(e)
+                # for i in range(0, imgheight, height):
+                for j in range(0, imgwidth-width, width):
+                    box = (j, 0, j + width, 0 + height)
+                    a = img.crop(box)
+                    try:
+                        o = a
+                        strfile = file.rstrip(".au.png")
+                        o.save(os.path.join(spectrogram_folder, strfile + "_" + str(j) + ".png"))
+                    except Exception as e:
+                        print(e)
 
 
 if __name__ == '__main__':
